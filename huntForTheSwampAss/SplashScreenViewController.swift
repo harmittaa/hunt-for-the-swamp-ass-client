898aaa88142ceacb5897ef02601b3915204e9023
iosdev <iosdev@mtk-idev-mba-14.local>
2016-04-26 11:06:15 +0300

    added background images, changed fonts and added back gamemode image

---
+++ huntForTheSwampAss/SplashScreenViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SplashScreenViewController.swift
+//  huntForTheSwampAss
+//
+//  Created by iosdev on 25.4.2016.
+//  Copyright © 2016 iosdev. All rights reserved.
+//
+
+import UIKit
+
+class SplashScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
