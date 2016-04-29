b61331b3c4efac83d2a881dca37d1a8ae5e8f058
iosdev <iosdev@mtk-idev-mba-14.local>
2016-04-29 17:18:13 +0300

    slight changes to main menu and login screen

--- huntForTheSwampAss/huntForTheSwampAss/UserProfileViewController.swift
+++ huntForTheSwampAss/huntForTheSwampAss/UserProfileViewController.swift
@@ -8,16 +8,26 @@
 
 import UIKit
 
-class UserProfileViewController: UIViewController {
+class UserProfileViewController: UIViewController,UITextFieldDelegate {
     @IBOutlet weak var loginPassword: UITextField!
     @IBOutlet weak var loginUsername: UITextField!
     @IBOutlet weak var signUpPassword: UITextField!
     @IBOutlet weak var signUpUsername: UITextField!
     @IBOutlet weak var loginButton: UIButton!
-    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var asd: UIImageView!
+    var activeField: UITextField?
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        loginPassword.delegate = self
+        loginUsername.delegate = self
+        signUpPassword.delegate = self
+        signUpUsername.delegate = self
+        //CALL this method to enable the view scrolling away from keyboard
+        registerForKeyboardNotifications()
+        //setting background
+        scrollView.backgroundColor = UIColor.clearColor()//UIColor(patternImage: UIImage(named: "hunts_bg")!)
+        asd.image = UIImage(CGImage: UIImage(named: "main_menu_bg")!.CGImage!, scale: 1.0, orientation: .DownMirrored)
         // Do any additional setup after loading the view.
     }
 
@@ -26,6 +36,10 @@ class UserProfileViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func loginPasswordEnterFunction(sender: AnyObject) {
+        print("[profiel] pressed enter")
+        self.view.endEditing(true)
+    }
     @IBAction func loginFunction(sender: UIButton) {
         
     }
@@ -41,5 +55,75 @@ class UserProfileViewController: UIViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    
+    /*
+ #BELOW ARE FUNCTIONS FOR MOCING VIEW OUT OF THE WAY OF THE KEYBOARD
+ #DO NOT TOUCH
+    */
+    
+    
+    func registerForKeyboardNotifications()
+    {
+        //Adding notifies on keyboard appearing
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWasShown:", name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyboardWillBeHidden:", name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    
+    func deregisterFromKeyboardNotifications()
+    {
+        //Removing notifies on keyboard appearing
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillHideNotification, object: nil)
+    }
+    
+    func keyboardWasShown(notification: NSNotification)
+    {
+        //Need to calculate keyboard exact size due to Apple suggestions
+        self.scrollView.scrollEnabled = true
+        var info : NSDictionary = notification.userInfo!
+        var keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue().size
+        var contentInsets : UIEdgeInsets = UIEdgeInsetsMake(0.0, 0.0, keyboardSize!.height, 0.0)
+        
+        self.scrollView.contentInset = contentInsets
+        self.scrollView.scrollIndicatorInsets = contentInsets
+        
+        var aRect : CGRect = self.view.frame
+        aRect.size.height -= keyboardSize!.height
+        if let activeFieldPresent = activeField
+        {
+            if (!CGRectContainsPoint(aRect, activeField!.frame.origin))
+            {
+                self.scrollView.scrollRectToVisible(activeField!.frame, animated: true)
+            }
+        }
+        
+        
+    }
+    
+    
+    func keyboardWillBeHidden(notification: NSNotification)
+    {
+        //Once keyboard disappears, restore original positions
+        var info : NSDictionary = notification.userInfo!
+        var keyboardSize = (info[UIKeyboardFrameBeginUserInfoKey] as? NSValue)?.CGRectValue().size
+        var contentInsets : UIEdgeInsets = UIEdgeInsetsMake(0.0, 0.0, -keyboardSize!.height, 0.0)
+        self.scrollView.contentInset = contentInsets
+        self.scrollView.scrollIndicatorInsets = contentInsets
+        self.view.endEditing(true)
+        self.scrollView.scrollEnabled = false
+        
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField!)
+    {
+        activeField = textField
+    }
+    
+    func textFieldDidEndEditing(textField: UITextField!)
+    {
+        activeField = nil
+    }
 
 }
